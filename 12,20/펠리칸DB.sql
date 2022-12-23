9f79c4b345bc37c167d9d2359abb6d35f9db12d2
dongyeonkim <ehd9567@naver.com>
2022-12-23 22:15:00 +0900

    관리자페이지 추가

--- 12,20/펠리칸DB.sql
+++ 12,20/펠리칸DB.sql
@@ -1,19 +1,34 @@
 use user1220;
 show tables;
-SELECT * FROM member;
-SELECT * FROM product;
-
-CREATE TABLE add_option(
-	option_num int not null primary key auto_increment,
-    carve varchar(20) null,
-    gift_wrapping varchar(10),
-    bag boolean not null,
-    message_card boolean not null
-);
-
-show tables;
 SELECT * FROM add_option;
-INSERT INTO add_option values(1,'DongYeon',true,false,'선물고마웡');
+desc add_option;
+use world;
+show tables;
+SELECT * FROM country;
+SELECT CountryCode FROM city where countryCode = 'KOR';
+SELECT distinct countryCode from city where countryCode = 'kor';
+SELECT * FROM city where countrycode = 'kor' and population >= 1000000;
+SELECT * FROM city where countrycode = 'kor' or countrycode = 'chn' or countrycode = 'jpn';
+SELECT * FROM city where countrycode != 'kor' and population >- 1000000;
+SELECT * FROM city where countrycode in ('kor','chn','jpn');
+SELECT * FROM city where countrycode = 'kor' order by population desc;
+SELECT countrycode,population FROM city order by countrycode,population desc;
+SELECT * FROM city where countrycode = 'kor' order by population desc limit 10;
+SELECT count(*) from city where countrycode = 'kor';
+SELECT sum(population) from city where countrycode = 'kor';
+select name,population from city 
+		where population > 
+			(select avg(population) from city where countrycode='kor') 
+		order by population desc;
 
-SELECT m.name,p.title FROM member as m join product as p on m.join_id = p.consumer_id;
-SELECT p.consumer_id,a.message_card FROM product as p join add_option as a on p.product_num = option_num;
+use user1220;
+CREATE TABLE manager (
+	manager_name varchar(20) not null,
+    manager_id	 varchar(20) not null primary key,
+    manager_password int not null,
+    manager_email varchar(100) not null,
+    manager_phone varchar(20) not null
+);
+SELECT * FROM manager;
+INSERT INTO manager values('이병헌','lee',1234,'lee@naver.com','010-956-7656');
+SELECT  mana.manager_num,mem.name FROM manager as mana join member as mem where mana.manager_num = mem.join_id;
