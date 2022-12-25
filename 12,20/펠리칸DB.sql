acb4fec8ea36f9d19ff6aaf16deb69b83c40fe78
dongyeonkim <ehd9567@naver.com>
2022-12-25 21:14:49 +0900

    sql명령어 연습

--- 12,20/펠리칸DB.sql
+++ 12,20/펠리칸DB.sql
@@ -1,34 +1,49 @@
-use user1220;
-show tables;
-SELECT * FROM add_option;
-desc add_option;
+show databases;
+show databases;
+use world;
 use world;
 show tables;
-SELECT * FROM country;
-SELECT CountryCode FROM city where countryCode = 'KOR';
-SELECT distinct countryCode from city where countryCode = 'kor';
-SELECT * FROM city where countrycode = 'kor' and population >= 1000000;
-SELECT * FROM city where countrycode = 'kor' or countrycode = 'chn' or countrycode = 'jpn';
-SELECT * FROM city where countrycode != 'kor' and population >- 1000000;
-SELECT * FROM city where countrycode in ('kor','chn','jpn');
-SELECT * FROM city where countrycode = 'kor' order by population desc;
-SELECT countrycode,population FROM city order by countrycode,population desc;
-SELECT * FROM city where countrycode = 'kor' order by population desc limit 10;
-SELECT count(*) from city where countrycode = 'kor';
-SELECT sum(population) from city where countrycode = 'kor';
-select name,population from city 
-		where population > 
-			(select avg(population) from city where countrycode='kor') 
-		order by population desc;
-
+show tables;
+desc city;
+desc city;
+select * from city;
+select * from city;
+select * from city where Countrycode = 'kor';
+select * from city where countrycode = 'kor';
+delete from city where id = 2; 
+delete from city where id = 4;
+select distinct name from city where countrycode = 'kor';
+select distinct name from city where countrycode = 'kor';
+select name,countrycode from city where countrycode = 'kor' and population >= 1000000;
+select name,countrycode from city where countrycode = 'kor' and population >= 1000000;
+select * from city where countrycode = 'kor' or countrycode = 'chn' or countrycode = 'jpn';
+select * from city where countrycode = 'kor' or countrycode = 'chn' or coyntrycode = 'jpn';
+select name from city where countrycode != 'kor' and population >= 1000000;
+select name from city where countrycode != 'kor' and population >= 1000000;
+select * from city where countrycode in ('kor','chn','jpn');
+select * from city where countrycode in ('kor','chn','jpn');
+select * from city where countrycode = 'kor' and (population >= 1000000 and population <= 5000000);
+select * from city where countrycode = 'kor' and (population >= 1000000 and population <= 5000000);
+select * from city where countrycode = 'kor' and (population between 1000000 and 5000000);
+select * from city where countrycode = 'kor' order by population desc;
+select * from city where countrycode = 'kor' order by population desc;
+select population,countrycode from city order by countrycode,population desc;
+select population,dountrycode from city order by countrycode,population desc;
+select * from city where countrycode='kor' order by population desc limit 10;
+select * from city where countrycode='kor' order by population desc limit 10;
+select count(*) from city where countrycode = 'kor';
+select count(*) from city where countrycode = 'kor';
+select sum(population) from city where countrycode = 'kor';
+select avg(population) from city where countrycode = 'kor';
+select min(population) from city where countrycode = 'kor';
+select length(name) from country;
+select upper(mid(name,1,3))from country;
 use user1220;
-CREATE TABLE manager (
-	manager_name varchar(20) not null,
-    manager_id	 varchar(20) not null primary key,
-    manager_password int not null,
-    manager_email varchar(100) not null,
-    manager_phone varchar(20) not null
-);
-SELECT * FROM manager;
-INSERT INTO manager values('이병헌','lee',1234,'lee@naver.com','010-956-7656');
-SELECT  mana.manager_num,mem.name FROM manager as mana join member as mem where mana.manager_num = mem.join_id;
+show tables;
+desc manager;
+desc add_option;
+select * from add_option;
+insert into add_option values(2,'ddong',true,true,'히히');
+update add_option set bag = true where option_num = 2;
+update add_option set message_card = '호호홓' where option_num = 2;
+select * from product join add_option where option_num = product_num;
